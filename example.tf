b47388162bffec4db715c9e5de49199acbe9d659
Sinh Van <Sinh.Van@vicinity.com.au>
2022-05-06 00:47:47 +1000

    Adding vNET

--- example.tf
+++ example.tf
@@ -17,3 +17,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "australiaeast"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "australiaeast"
+    resource_group_name = azurerm_resource_group.rg.name
+}
